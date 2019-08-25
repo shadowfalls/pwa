850dbe76a6a075dc3b500a9fca354d7561d91f16
shadowfalls <dinesh.muralidharan@gmail.com>
2019-08-25 11:24:38 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -6,28 +6,6 @@ var URLS = [                            // Add URL you want to cache in this lis
   '/pwa/index.html'            // add path to those files here
 ]
 
-window.addEventListener('beforeinstallprompt', (e) => {
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
-  // Update UI notify the user they can add to home screen
-  // Show the prompt
-  deferredPrompt.prompt();
-  // Wait for the user to respond to the prompt
-  deferredPrompt.userChoice
-    .then((choiceResult) => {
-      if (choiceResult.outcome === 'accepted') {
-        console.log('User accepted the A2HS prompt');
-      } else {
-        console.log('User dismissed the A2HS prompt');
-      }
-      deferredPrompt = null;
-    });
-});
-
-window.addEventListener('appinstalled', (evt) => {
-  console.log('a2hs installed');
-});
-
 // Respond with cached resources
 self.addEventListener('fetch', function (e) {
   console.log('fetch request : ' + e.request.url)
